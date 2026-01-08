2fa113f8e61cb6dbf02a5a83a449e526dddbb083
monster <joycechen.event@gmail.com>
2026-01-08 23:54:41 +0800

    feat: 預備部署 gh-page

--- src/App.jsx
+++ src/App.jsx
@@ -162,7 +162,10 @@ function App() {
                     alt="產品主圖"
                   />
                   <div className="card-body">
-                    <h5 className="card-title">{tempProduct.title}</h5>
+                    <h5 className="card-title">
+                      {tempProduct.title}
+                      <span className="badge bg-primary ms-2">{tempProduct.category}</span>
+                    </h5>
                     <p className="card-text">商品描述：{tempProduct.description}</p>
                     <p className="card-text">商品內容：{tempProduct.content}</p>
                     <div className="d-flex mb-3">
